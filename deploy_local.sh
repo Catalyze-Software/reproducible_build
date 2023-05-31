e9e990ec5f5e06e60e96b7e11226e290ee8561c4
Jonathan Green <dev.jonathan.green@gmail.com>
2023-05-31 18:58:37 -0400

    renmaed script file and added dfx check

---
+++ deploy_local.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+if ! command -v dfx /dev/null
+then
+    echo "dfx could not be found"
+    echo "installing dfx"
+    sh -ci "$(curl -fsSL https://sdk.dfinity.org/install.sh)"
+fi
+
+dfx stop
+dfx start --background --clean
+
+dfx deploy sns_event_attendees
+dfx deploy sns_events
+dfx deploy sns_groups
+dfx deploy sns_members
+dfx deploy sns_profiles
+dfx deploy sns_reports
--- start.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-sh -ci "$(curl -fsSL https://sdk.dfinity.org/install.sh)"
-#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs >> rustup.sh
-#sh rustup.sh -y
-#rm -rf rustup.sh
-#rustup target add wasm32-unknown-unknown
-dfx start --background
-dfx deploy parent
